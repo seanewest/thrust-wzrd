f94e59b4bc888c72a0837f169d0050a9b5521bb5
Sean West <sean.e.west@gmail.com>
2014-12-25 13:00:27 -0500

    use node-thrust directly

--- cmd.js
+++ cmd.js
@@ -1,23 +1,42 @@
 #!/usr/bin/env node
 
 var wzrd = require('wzrd');
-var thrustWindow = require('thrust-window');
+var thrust = require('node-thrust');
+
 var width = 1024;
 var height = 600;
 var port = 9966;
 var entry = process.argv[2];
-var wzrd_opts = {entries: [ { from: entry, to: entry } ] }
-
 if (process.argv[3])
   width = parseInt(process.argv[3]);
-
 if (process.argv[4])
   height = parseInt(process.argv[4]);
 
-wzrd.http(wzrd_opts).listen(port, function(err) {
+var opts = {}
+opts.entries = [ {from: entry, to: entry} ];
+var server = wzrd.http(opts);
+
+server.listen(port, function(err) {
   if (err) {
-    console.log(err);
-  } else {
-    thrustWindow("http://localhost:9966", width, height);
+    console.error('error starting server', err)
+    process.exit(1)
   }
+
+  thrust(function(err, api) {
+    if (err) {
+      console.error('error starting thrust', err)
+      process.exit(1)
+    }
+
+    var local_url = 'http://localhost:' + port;
+    var win = api.window({
+      root_url: local_url,
+      size: {
+        width: width,
+        height: height
+      }
+    })
+    win.show();
+    win.focus();
+  });
 })
